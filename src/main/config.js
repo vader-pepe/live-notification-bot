d5e13d8deeaae6d59f6b4504620db7170d34c410
Ihsan Kurniawan <ihsan@localhost.localdomain>
2025-01-30 20:33:46 +0700

    feat: initial commit for typescript

--- src/main/config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-require("dotenv").config();
-
-module.exports = {
-  botAvatarUrl:
-    "https://cdn.discordapp.com/attachments/950307623159365646/1264436643687960596/Fiony_Enhance.png?ex=669ddde3&is=669c8c63&hm=cdfe7675d7ddcea138d9a86404d4aa5458db66d1c648fa1c316381e7dbbfa61d&",
-  port: process.env.PORT,
-  ipAddress: process.env.IP_ADDRESS
-};
